54a0a6df7529dd6d76b77e6a0e1b878630aec1e5
Rodriguez Ordoñez Juan Daniel <juan.rodriguez.o@tecsup.edu.pe>
2025-06-04 15:26:09 -0500

    Update Hero.jsx

--- src/components/modules/Hero.jsx
+++ src/components/modules/Hero.jsx
@@ -1,17 +1,17 @@
 const Hero = () => {
     return(
         <section className="px-4 pt-5 my-5 text-center border-bottom">
-            <h1 className="display-4 fw-bold">Centered screenshot</h1>
+            <h1 className="display-4 fw-bold">Netflix</h1>
             <div className="col-lg-6 mx-auto">
-            <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+            <p className="lead mb-4"></p>
             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-                <button type="button" className="btn btn-primary btn-lg px-4 me-sm-3">Primary button</button>
-                <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+                <button type="button" className="btn btn-primary btn-lg px-4 me-sm-3">Ver Ahora</button>
+                <button type="button" className="btn btn-outline-secondary btn-lg px-4">Mi Lista</button>
             </div>
             </div>
             <div className="overflow-hidden">
             <div className="container px-5">
-                <img src="bootstrap-docs.png" className="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy" />
+                <img src="https://hips.hearstapps.com/vidthumb/images/oppenheimer-cillian-murphy-6458ae16ab87c.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*" className="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy" />
             </div>
             </div>
         </section>
